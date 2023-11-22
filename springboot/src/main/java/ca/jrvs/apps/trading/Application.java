a16e17f790c4fa0fe22776307d307e251f685dce
bbrayan <brayanboukhman@yahoo.ca>
2023-11-22 00:51:41 -0500

    init springboot

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/Application.java
@@ -0,0 +1,9 @@
+package ca.jrvs.apps.trading;
+
+public class Application {
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[]) {
+        System.out.println("Hello, World");
+    }
+}
